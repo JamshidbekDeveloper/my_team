c17368c86c013c5227ea8a0952a780fe2e7875ba
_anvarovnaz_ <macstore.uz@MacBook-Pro-anvarovnaz.local>
2024-12-18 17:10:28 -0800

    Added page8

---
+++ lib/page8.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class Page8 extends StatelessWidget {
+  const Page8({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
